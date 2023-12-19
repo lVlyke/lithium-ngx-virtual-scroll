6b43e2cda88b17fa9a7392c575bf2237e17434b2
Mychal Thompson <mychalrthompson@gmail.com>
2023-12-19 15:50:37 -0500

    0.3.1: - Added support for Angular 17. - Replaced yarn with npm. - Replaced istanbul with c8.

--- scripts/test.js
+++ scripts/test.js
@@ -3,8 +3,25 @@
 
 import child_process from "child_process";
 
-(function main() {
-    child_process.execSync("NODE_OPTIONS=--es-module-specifier-resolution=node istanbul cover --root ./spec/build/src --include-all-sources --report cobertura --report lcov jasmine", { stdio: "inherit" });
+const [
+    NODE_MAJOR_VER,
+    NODE_MINOR_VER
+] = process.versions.node.split(".").map(Number);
 
-    child_process.execSync("istanbul-cobertura-badger -b coverage -e 90 coverage/cobertura.xml -d coverage/", { stdio: "inherit" });
+(function main() {
+    // Determine which Node flags to pass to allow extensionless imports
+    let NODE_OPTS;
+    if (NODE_MAJOR_VER < 19) {
+        NODE_OPTS = "--es-module-specifier-resolution=node";
+    } else if (NODE_MAJOR_VER <= 20 && NODE_MINOR_VER < 6) {
+        NODE_OPTS = "--experimental-loader=extensionless";
+    } else {
+        NODE_OPTS = "--import=extensionless/register";
+    }
+
+    // Run tests and generate coverage reports
+    child_process.execSync(`NODE_OPTIONS=${NODE_OPTS} npx c8 jasmine`, { stdio: "inherit" });
+
+    // Generate coverage badge
+    child_process.execSync("npx istanbul-cobertura-badger -b coverage -e 90 -r coverage/cobertura-coverage.xml -d coverage/", { stdio: "inherit" });
 })();
